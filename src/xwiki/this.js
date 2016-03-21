a7effc3a078d1dd60a8cb871c3893fc72535b65e
ansuz <ansuz@transitiontech.ca>
2016-03-21 15:54:36 +0100

    leave head in snapshot state

--- src/xwiki/this.js
+++ src/xwiki/this.js
@@ -1,5 +1,5 @@
 Package(function (pkg, XWiki) {
-    pkg.setXarName('realtime-wysiwyg-1.8.xar');
+    pkg.setXarName('realtime-wysiwyg-1.8-SNAPSHOT.xar');
     pkg.setName("XWiki - Contrib - RealtimeWysiwyg");
     pkg.setDescription("A realtime collaborative WYSIWYG editor extension");
     pkg.setExtensionId("org.xwiki.contrib:xwiki-contrib-realtimewysiwyg");
