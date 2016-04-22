c5a1876b1e80122ad775c22414ace6fc758510b7
Caleb James DeLisle <cjd@cjdns.fr>
2016-04-22 18:35:45 +0200

    Next release will be 16

--- src/xwiki/this.js
+++ src/xwiki/this.js
@@ -1,5 +1,5 @@
 Package(function (pkg, XWiki) {
-    pkg.setXarName('realtime-wysiwyg-1.15.xar');
+    pkg.setXarName('realtime-wysiwyg-1.16.xar');
     pkg.setName("XWiki - Contrib - RealtimeWysiwyg");
     pkg.setDescription("A realtime collaborative WYSIWYG editor extension");
     pkg.setExtensionId("org.xwiki.contrib:xwiki-contrib-realtimewysiwyg");
