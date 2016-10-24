7acd68818c309c7f55297815c98c7e68cb4cf019
yflory <yann.flory@xwiki.com>
2016-10-24 14:35:56 +0200

    Set the new version

--- src/xwiki/this.js
+++ src/xwiki/this.js
@@ -1,5 +1,5 @@
 Package(function (pkg, XWiki) {
-    pkg.setXarName('realtime-wysiwyg-1.21.xar');
+    pkg.setXarName('realtime-wysiwyg-1.22.xar');
     pkg.setName("XWiki - Contrib - RealtimeWysiwyg");
     pkg.setDescription("A realtime collaborative WYSIWYG editor extension");
     pkg.setExtensionId("org.xwiki.contrib:xwiki-contrib-realtimewysiwyg");
